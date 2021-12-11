070c1fa85fd6701a6e1f6344ad4cce9d89b9787d
wp_xyz <wp_xxyyzzz@gmx.net>
2021-12-11 12:21:27 +0100

    Initial commit. Improved layout.

--- umainform.pas
+++ umainform.pas
@@ -13,36 +13,36 @@ type
   { TForm1 }
 
   TForm1 = class(TForm)
-    CheckBox1: TCheckBox;
-    CheckBox2: TCheckBox;
+    cbDrawText: TCheckBox;
+    cbWhiteBackground: TCheckBox;
     GroupBox1: TGroupBox;
     Label1: TLabel;
     Label2: TLabel;
     Label3: TLabel;
-    LabeledEdit1: TLabeledEdit;
+    Label4: TLabel;
+    edDetailSize: TEdit;
     ListBox1: TListBox;
     ListBox2: TListBox;
-    PaintBox1: TPaintBox;
-    SpinEdit1: TSpinEdit;
-    SpinEdit2: TSpinEdit;
+    PaintBox: TPaintBox;
+    seCustomWidth: TSpinEdit;
+    seCustomHeight: TSpinEdit;
     Splitter1: TSplitter;
     Splitter2: TSplitter;
     Splitter3: TSplitter;
-    procedure PBpaint(Sender: TObject);
-    procedure SelectSubItem(Sender: TObject; User: boolean);
     procedure _dtchange(Sender: TObject);
-    procedure _onCreate(Sender: TObject);
-    procedure _SelectMainItem(Sender: TObject; User: boolean);
-    procedure FillListBox(LBox: TListBox; pti: PTypeInfo);
+    procedure FormCreate(Sender: TObject);
+    procedure PaintboxPaint(Sender: TObject);
+    procedure SelectMainItem(Sender: TObject; {%H-}User: boolean);
+    procedure SelectSubItem(Sender: TObject; {%H-}User: boolean);
   private
-    DetailName: string;
-    pti: PTypeInfo;
-    td:TThemedElementDetails;
-    dsize:TSize;
-    selected:boolean;
-    { private declarations }
+    FDetailName: string;
+    FTypeInfo: PTypeInfo;
+    FDetails: TThemedElementDetails;
+    FDetailSize: TSize;
+    FSelected: boolean;
+    procedure FillListBox(LBox: TListBox; pti: PTypeInfo);
+    procedure UpdatePaintbox(AllSelected: Boolean);
   public
-    { public declarations }
   end;
 
 var
@@ -53,146 +53,157 @@ implementation
 {$R *.lfm}
 
 { TForm1 }
+
 procedure TForm1.FillListBox(LBox: TListBox; pti: PTypeInfo);
 var
-  aname: string;
-  i: integer;
+  s: string;
+  i: PtrInt;
 begin
   LBox.Clear;
   for i:=0 to GetEnumNameCount(pti)-1 do
   begin
-       aname := GetEnumName(pti, i);
-       LBox.AddItem(aname,tobject(pointer(i)));
+    s := GetEnumName(pti, i);
+    LBox.AddItem(s, TObject({%H-}Pointer(i)));
   end;
 end;
 
-procedure TForm1._onCreate(Sender: TObject);
+procedure TForm1.FormCreate(Sender: TObject);
 begin
-  selected:=false;
-  FillListBox(ListBox1,TypeInfo(TThemedElement));
+  FSelected := false;
+  FillListBox(ListBox1, TypeInfo(TThemedElement));
 end;
 
-function GetTI(TypeName:string): PTypeInfo;
+function GetTI(TypeName: string): PTypeInfo;
 begin
-     case TypeName of
-        'teButton':result:=TypeInfo(TThemedButton);
-        'teClock':result:=TypeInfo(TThemedClock);
-        'teComboBox':result:=TypeInfo(TThemedComboBox);
-        'teEdit':result:=TypeInfo(TThemedEdit);
-        'teExplorerBar':result:=TypeInfo(TThemedExplorerBar);
-        'teHeader':result:=TypeInfo(TThemedHeader);
-        'teListView':result:=TypeInfo(TThemedListView);
-        'teMenu':result:=TypeInfo(TThemedMenu);
-        'tePage':result:=TypeInfo(TThemedPage);
-        'teProgress':result:=TypeInfo(TThemedProgress);
-        'teRebar':result:=TypeInfo(TThemedRebar);
-        'teScrollBar':result:=TypeInfo(TThemedScrollBar);
-        'teSpin':result:=TypeInfo(TThemedSpin);
-        'teStartPanel':result:=TypeInfo(TThemedStartPanel);
-        'teStatus':result:=TypeInfo(TThemedStatus);
-        'teTab':result:=TypeInfo(TThemedTab);
-        'teTaskBand':result:=TypeInfo(TThemedTaskBand);
-        'teTaskBar':result:=TypeInfo(TThemedTaskBar);
-        'teToolBar':result:=TypeInfo(TThemedToolBar);
-        'teToolTip':result:=TypeInfo(TThemedToolTip);
-        'teTrackBar':result:=TypeInfo(TThemedTrackBar);
-        'teTrayNotify':result:=TypeInfo(TThemedTrayNotify);
-        'teTreeview':result:=TypeInfo(TThemedTreeview);
-        'teWindow':result:=TypeInfo(TThemedWindow);
-     end;
+  case TypeName of
+    'teButton': Result := TypeInfo(TThemedButton);
+    'teClock': Result := TypeInfo(TThemedClock);
+    'teComboBox': Result := TypeInfo(TThemedComboBox);
+    'teEdit': Result := TypeInfo(TThemedEdit);
+    'teExplorerBar': Result := TypeInfo(TThemedExplorerBar);
+    'teHeader': Result := TypeInfo(TThemedHeader);
+    'teListView': Result := TypeInfo(TThemedListView);
+    'teMenu': Result := TypeInfo(TThemedMenu);
+    'tePage': Result := TypeInfo(TThemedPage);
+    'teProgress': Result := TypeInfo(TThemedProgress);
+    'teRebar': Result := TypeInfo(TThemedRebar);
+    'teScrollBar': Result := TypeInfo(TThemedScrollBar);
+    'teSpin': Result := TypeInfo(TThemedSpin);
+    'teStartPanel': Result := TypeInfo(TThemedStartPanel);
+    'teStatus': Result:= TypeInfo(TThemedStatus);
+    'teTab': Result := TypeInfo(TThemedTab);
+    'teTaskBand': Result := TypeInfo(TThemedTaskBand);
+    'teTaskBar': Result := TypeInfo(TThemedTaskBar);
+    'teToolBar': Result := TypeInfo(TThemedToolBar);
+    'teToolTip': Result := TypeInfo(TThemedToolTip);
+    'teTrackBar': Result := TypeInfo(TThemedTrackBar);
+    'teTrayNotify': Result := TypeInfo(TThemedTrayNotify);
+    'teTreeview': Result := TypeInfo(TThemedTreeview);
+    'teWindow': Result := TypeInfo(TThemedWindow);
+  end;
 end;
-function GetTDetail(TypeName:string;Element:integer):TThemedElementDetails;
+
+function GetTDetail(TypeName: string; Element: integer): TThemedElementDetails;
 begin
-     case TypeName of
-        'teButton':result:=ThemeServices.GetElementDetails(TThemedButton(Element));
-        'teClock':result:=ThemeServices.GetElementDetails(TThemedClock(Element));
-        'teComboBox':result:=ThemeServices.GetElementDetails(TThemedComboBox(Element));
-        'teEdit':result:=ThemeServices.GetElementDetails(TThemedEdit(Element));
-        'teExplorerBar':result:=ThemeServices.GetElementDetails(TThemedExplorerBar(Element));
-        'teHeader':result:=ThemeServices.GetElementDetails(TThemedHeader(Element));
-        'teListView':result:=ThemeServices.GetElementDetails(TThemedListView(Element));
-        'teMenu':result:=ThemeServices.GetElementDetails(TThemedMenu(Element));
-        'tePage':result:=ThemeServices.GetElementDetails(TThemedPage(Element));
-        'teProgress':result:=ThemeServices.GetElementDetails(TThemedProgress(Element));
-        'teRebar':result:=ThemeServices.GetElementDetails(TThemedRebar(Element));
-        'teScrollBar':result:=ThemeServices.GetElementDetails(TThemedScrollBar(Element));
-        'teSpin':result:=ThemeServices.GetElementDetails(TThemedSpin(Element));
-        'teStartPanel':result:=ThemeServices.GetElementDetails(TThemedStartPanel(Element));
-        'teStatus':result:=ThemeServices.GetElementDetails(TThemedStatus(Element));
-        'teTab':result:=ThemeServices.GetElementDetails(TThemedTab(Element));
-        'teTaskBand':result:=ThemeServices.GetElementDetails(TThemedTaskBand(Element));
-        'teTaskBar':result:=ThemeServices.GetElementDetails(TThemedTaskBar(Element));
-        'teToolBar':result:=ThemeServices.GetElementDetails(TThemedToolBar(Element));
-        'teToolTip':result:=ThemeServices.GetElementDetails(TThemedToolTip(Element));
-        'teTrackBar':result:=ThemeServices.GetElementDetails(TThemedTrackBar(Element));
-        'teTrayNotify':result:=ThemeServices.GetElementDetails(TThemedTrayNotify(Element));
-        'teTreeview':result:=ThemeServices.GetElementDetails(TThemedTreeview(Element));
-        'teWindow':result:=ThemeServices.GetElementDetails(TThemedWindow(Element));
-     end;
+  case TypeName of
+    'teButton': Result := ThemeServices.GetElementDetails(TThemedButton(Element));
+    'teClock': Result := ThemeServices.GetElementDetails(TThemedClock(Element));
+    'teComboBox': Result := ThemeServices.GetElementDetails(TThemedComboBox(Element));
+    'teEdit': Result := ThemeServices.GetElementDetails(TThemedEdit(Element));
+    'teExplorerBar': Result := ThemeServices.GetElementDetails(TThemedExplorerBar(Element));
+    'teHeader': Result := ThemeServices.GetElementDetails(TThemedHeader(Element));
+    'teListView': Result := ThemeServices.GetElementDetails(TThemedListView(Element));
+    'teMenu': Result := ThemeServices.GetElementDetails(TThemedMenu(Element));
+    'tePage': Result := ThemeServices.GetElementDetails(TThemedPage(Element));
+    'teProgress': Result := ThemeServices.GetElementDetails(TThemedProgress(Element));
+    'teRebar': Result := ThemeServices.GetElementDetails(TThemedRebar(Element));
+    'teScrollBar': Result := ThemeServices.GetElementDetails(TThemedScrollBar(Element));
+    'teSpin': Result := ThemeServices.GetElementDetails(TThemedSpin(Element));
+    'teStartPanel':result:=ThemeServices.GetElementDetails(TThemedStartPanel(Element));
+    'teStatus': Result := ThemeServices.GetElementDetails(TThemedStatus(Element));
+    'teTab': Result := ThemeServices.GetElementDetails(TThemedTab(Element));
+    'teTaskBand': Result := ThemeServices.GetElementDetails(TThemedTaskBand(Element));
+    'teTaskBar': Result := ThemeServices.GetElementDetails(TThemedTaskBar(Element));
+    'teToolBar': Result := ThemeServices.GetElementDetails(TThemedToolBar(Element));
+    'teToolTip': Result := ThemeServices.GetElementDetails(TThemedToolTip(Element));
+    'teTrackBar': Result := ThemeServices.GetElementDetails(TThemedTrackBar(Element));
+    'teTrayNotify': Result := ThemeServices.GetElementDetails(TThemedTrayNotify(Element));
+    'teTreeview': Result := ThemeServices.GetElementDetails(TThemedTreeview(Element));
+    'teWindow': Result := ThemeServices.GetElementDetails(TThemedWindow(Element));
+  end;
 end;
-procedure TForm1._SelectMainItem(Sender: TObject; User: boolean);
-var
-  i: integer;
+
+procedure TForm1.SelectMainItem(Sender: TObject; User: boolean);
 begin
-     DetailName:=GetEnumName(TypeInfo(TThemedElement),ListBox1.ItemIndex);
-     pti:=GetTI(DetailName);
-     FillListBox(ListBox2,pti);
+  FDetailName := GetEnumName(TypeInfo(TThemedElement), ListBox1.ItemIndex);
+  FTypeInfo := GetTI(FDetailName);
+  FillListBox(ListBox2, FTypeInfo);
+  UpdatePaintbox(false);
 end;
+
 procedure TForm1.SelectSubItem(Sender: TObject; User: boolean);
 begin
-    td:=GetTDetail(DetailName,ListBox2.ItemIndex);
-    dsize:=ThemeServices.GetDetailSize(td);
-    LabeledEdit1.Caption:='cx='+inttostr(dsize.cx)+'; cy='+inttostr(dsize.cy);
-    selected:=true;
-    self.PaintBox1.Invalidate;
+  FDetails := GetTDetail(FDetailName, ListBox2. ItemIndex);
+  FDetailSize := ThemeServices.GetDetailSize(FDetails);
+  edDetailSize.Caption := Format('cx = %d; cy = %d', [FDetailSize.cx, FDetailSize.cy]);
+  UpdatePaintbox(true);
 end;
 
 procedure TForm1._dtchange(Sender: TObject);
 begin
-     self.PaintBox1.Invalidate;
+  PaintBox.Invalidate;
 end;
 
-procedure TForm1.PBpaint(Sender: TObject);
+procedure TForm1.PaintboxPaint(Sender: TObject);
 var
-   ARect:TRect;
-   dt:boolean;
-   tc:tcolor;
+  R: TRect;
+  drawTxt: boolean;
+  clr: TColor;
 begin
-     if CheckBox2.Checked then
-     begin
-          tc:=PaintBox1.Canvas.Brush.Color;
-          PaintBox1.Canvas.Brush.Color:=clWhite;
-          PaintBox1.Canvas.FillRect(PaintBox1.BoundsRect);
-          PaintBox1.Canvas.Brush.Color:=tc;
-     end;
-     if selected then
-     begin
-          dt:=CheckBox1.State=cbChecked;
-          ARect:=rect(10,10,10+dsize.cx,10+dsize.cy);
-          ThemeServices.DrawElement(PaintBox1.Canvas.Handle,td, ARect);
-          if dt then
-          ThemeServices.DrawText(PaintBox1.Canvas.Handle,td,'preferred',ARect,DT_CENTER or DT_VCENTER,0);
-          ARect:=rect(100,10,100+16,10+16);
-          ThemeServices.DrawElement(PaintBox1.Canvas.Handle,td, ARect);
-          if dt then
-          ThemeServices.DrawText(PaintBox1.Canvas.Handle,td,'16x16',ARect,DT_CENTER or DT_VCENTER,0);
-          ARect:=rect(200,10,200+32,10+32);
-          ThemeServices.DrawElement(PaintBox1.Canvas.Handle,td, ARect);
-          if dt then
-          ThemeServices.DrawText(PaintBox1.Canvas.Handle,td,'32x32',ARect,DT_CENTER or DT_VCENTER,0);
-          ARect:=rect(300,10,300+64,10+64);
-          ThemeServices.DrawElement(PaintBox1.Canvas.Handle,td, ARect);
-          if dt then
-          ThemeServices.DrawText(PaintBox1.Canvas.Handle,td,'64x64',ARect,DT_CENTER or DT_VCENTER,0);
+  if cbWhiteBackground.Checked then
+  begin
+    clr:= PaintBox.Canvas.Brush.Color;
+    PaintBox.Canvas.Brush.Color := clWhite;
+    PaintBox.Canvas.FillRect(0, 0, Paintbox.Width, Paintbox.Height);
+    PaintBox.Canvas.Brush.Color := clr;
+  end;
+  
+  if FSelected then
+  begin
+    drawTxt := cbDrawText.State=cbChecked;
+    R := Rect(10, 10, 10+FDetailSize.cx, 10+FDetailSize.cy);
+    ThemeServices.DrawElement(PaintBox.Canvas.Handle, FDetails, R);
+    if drawTxt then
+      ThemeServices.DrawText(PaintBox.Canvas.Handle, FDetails, 'preferred', R, DT_CENTER or DT_VCENTER, 0);
+    
+    R := Rect(100,10, 100+16, 10+16);
+    ThemeServices.DrawElement(PaintBox.Canvas.Handle, FDetails, R);
+    if drawTxt then
+      ThemeServices.DrawText(PaintBox.Canvas.Handle, FDetails, '16x16', R, DT_CENTER or DT_VCENTER, 0);
 
-          ARect:=rect(400,10,400+SpinEdit1.Value,10+SpinEdit2.Value);
-          ThemeServices.DrawElement(PaintBox1.Canvas.Handle,td, ARect);
-          if dt then
-          ThemeServices.DrawText(PaintBox1.Canvas.Handle,td,inttostr(SpinEdit1.Value)+'x'+inttostr(SpinEdit2.Value),ARect,DT_CENTER or DT_VCENTER,0);
+    R := Rect(200, 10, 200+32, 10+32);
+    ThemeServices.DrawElement(PaintBox.Canvas.Handle, FDetails, R);
+    if drawTxt then
+      ThemeServices.DrawText(PaintBox.Canvas.Handle, FDetails, '32x32', R, DT_CENTER or DT_VCENTER, 0);
 
-     end;
+    R := Rect(300, 10, 300+64, 10+64);
+    ThemeServices.DrawElement(PaintBox.Canvas.Handle, FDetails, R);
+    if drawTxt then
+      ThemeServices.DrawText(PaintBox.Canvas.Handle, FDetails, '64x64', R, DT_CENTER or DT_VCENTER, 0);
+  
+    R := Rect(400, 10, 400+seCustomWidth.Value, 10+seCustomHeight.Value);
+    ThemeServices.DrawElement(PaintBox.Canvas.Handle, FDetails, R);
+    if drawTxt then
+      ThemeServices.DrawText(PaintBox.Canvas.Handle, FDetails, Format('%dx%d', [seCustomWidth.Value, seCustomHeight.Value]), R, DT_CENTER or DT_VCENTER, 0);
+  end;
+end;
+
+procedure TForm1.UpdatePaintbox(AllSelected: Boolean);
+begin
+  FSelected := AllSelected;
+  Paintbox.Invalidate;
 end;
 
 
 end.
-
+
